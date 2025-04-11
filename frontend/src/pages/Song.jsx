dec9ade9581a5c6eee099a61147d9ad71381d72d
Nicky <nicolasyanase18@gmail.com>
2025-04-11 19:46:12 -0300

    feat: novas funcionalidades de ver detalhes da musica

--- frontend/src/pages/Song.jsx
+++ frontend/src/pages/Song.jsx
@@ -1,7 +1,25 @@
-import React from 'react'
+import React from "react";
 
-export const Song = () => {
+const Song = () => {
   return (
-    <div>Song</div>
-  )
-}
+    <div className="song">
+      <div className="song__container">
+        <div className="song__image-container">
+          <img
+            src="https://i.scdn.co/image/ab67616d00001e02af41105c5cd91b28f2cf219d"
+            alt="imagem da música X"
+          />
+        </div>
+      </div>
+      <div className="song__bar">
+        <div className="song__artist-image">
+          <img src="https://i.scdn.co/image/ab6761610000517499c26e8fb91634b00764bbb9" alt="imagem do artista X" />
+        </div>
+        <div className="player"></div>
+        <div></div>
+      </div>
+    </div>
+  );
+};
+
+export default Song;
