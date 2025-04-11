327c04f98d965178ed06129dc03273fb6de17203
Nicky <nicolasyanase18@gmail.com>
2025-04-11 18:34:33 -0300

    frontend finalizado

--- frontend/src/components/ItemList.jsx
+++ frontend/src/components/ItemList.jsx
@@ -3,45 +3,34 @@ import SingleItem from "./SingleItem";
 import { Link, useLocation } from "react-router-dom";
 
 const ItemList = ({ items, title, itemsArray,  path, idPath }) => {
-  const {pathName} = useLocation() //pega o caminho do arquivo para podemos renderizar mais itens nesta url
+  const {pathname} = useLocation() //pega o caminho do arquivo para podemos renderizar mais itens nesta url
+
+  const isHome = pathname === "/"; 
+
+  const finalItems = isHome ? items : Infinity;
   return (
     <div className="item-list">
-      <div className="item-list__header">
-        <h2>{title}</h2>
-        <Link className="item-list__link" to={path}>
-          Mostrar tudo
-        </Link>
-      </div>
+      {isHome && (
+          <div className="item-list__header">
+            <h2>{title}</h2>
+            <Link className="item-list__link" to={path}>
+              Mostrar tudo
+            </Link>
+          </div>
+      )}
 
       <div className="item-list__container">
         {itemsArray
           .filter((currentValue, index) => 
-            index < items
+            index < finalItems
           )
           .map((currObj, index) => (
             <SingleItem
               key={`${index}-${title}`}
               idPath={idPath}
               {...currObj}
-              // id={artist.id}
-              // name={artist.name}
-              // image={artist.image}
-              // banner={artist.banner}
-
             />
           ))}
-        {/* 
-        essa função servia para renderizar os componentes com dados fixos, só para preencher espaço no front
-
-        com essa função Criamos um array com numero de Items passado como props, .fill fazemos com que esse array fique preechido com valores "undefined e .map() renderiza cada singleItem no array, sobrepondo os valores undefined" 
-
-        {Array(items)
-          .fill()
-          .map((__, index) => (
-            <SingleItem key={`${title}-${index}`} />
-          ))}
-        
-        a "key" auxilia o react em rederizações, Ficará artista10, */}
       </div>
     </div>
   );
